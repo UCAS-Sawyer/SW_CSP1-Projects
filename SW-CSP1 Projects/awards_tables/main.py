424bac5b8e94c238085d38b22cbffc7926b18ef1
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-05 09:08:25 -0600

    Add files via upload

---
+++ SW-CSP1 Projects/awards_tables/main.py
@@ -0,0 +1,13 @@
+#Sawyer Wood, Variables..
+
+students = 100
+students = students-1
+seats = 12
+staff = 32
+staff = staff-3
+numberofguestsperstudent = ((students-15)*2)+15
+schoolboardmember = 1
+
+
+
+print((staff+students+numberofguestsperstudent+schoolboardmember)/seats)
---
+++ SW-CSP1 Projects/hello_world1/main.py
@@ -0,0 +1,5 @@
+#Sawyer Wood, Hello World
+
+name = input("What is your name?: ")
+
+print("Hello", name)
